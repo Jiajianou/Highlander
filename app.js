c0fcfbabea1688b6b5913b7755c500923283dfeb
J <Jiajian.ou@gmail.com>
2018-08-28 15:25:46 -0400

    adding views folder

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine","ejs");
+
+app.get("/", function(req, res){
+  console.log("someone has made a request call");
+  res.render("index2");
+
+});
+
+app.listen(3001);
+
+console.log("server is running");
